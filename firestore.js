819a8550b9db8477cce3c65da13f0182211b76e0
hosan <fbghtks1000@gmail.com>
2024-02-08 20:31:04 +0900

    please

--- firestore.js
+++ firestore.js
@@ -1,5 +1,5 @@
 // firestore.js
-import { app } from "./firebase";
+import app from "./firebase"; // 수정된 부분
 import { getFirestore } from "firebase/firestore";
 
 const fireStore = getFirestore(app);
